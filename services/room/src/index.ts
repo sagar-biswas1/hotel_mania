6151c708743ee1751eef92cb058aa1a3cb3ca77e
sagar-biswas1 <bsagor99@gmail.com>
2024-05-24 15:11:26 +0600

    minor change in room service

--- services/room/src/index.ts
+++ services/room/src/index.ts
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 
 //health
 app.get('/health', (_req, res) => {
-	res.status(200).json({ status: 'UP' });
+	res.status(200).json({ status: 'UP....' });
 });
 
 app.use((req, res, next) => {
