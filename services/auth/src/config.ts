14a5a48fa427f1aa890df45f43ab6da4dab27454
sagar-biswas1 <bsagor99@gmail.com>
2024-05-24 17:14:00 +0600

    passing env in workflows

--- services/auth/src/config.ts
+++ services/auth/src/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const USER_SERVICE =
-	process.env.USER_SERVICE_URL || 'http://localhost:4000';
+	process.env.USER_SERVICE_URL || 'https://user-drab.vercel.app';
 
 export const EMAIL_SERVICE =
-	process.env.EMAIL_SERVICE_URL || 'http://localhost:4005';
+	process.env.EMAIL_SERVICE_URL || 'https://email-three-phi.vercel.app';
